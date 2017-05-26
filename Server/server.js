be4d5a44464383205e8584ec9cb2f78f1669e021
ABS User <ABS User>
2017-05-26 10:05:10 +0100

    troubleshoot hound

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import controllers from './controllers/postItController';
 
-const app = express();
+var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
