3eba000bc558a6d3f5d516ad7576565280799f82
Nnamso <andeladeveloper@Nnamso.local>
2017-11-09 02:27:49 +0100

    bug-fix/implement-defense-feedback
    Fix dotenv webpack issue

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,11 @@ const config = {
     new Dotenv({
       path: '.env',
       safe: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
     })
   ],
   node: {
